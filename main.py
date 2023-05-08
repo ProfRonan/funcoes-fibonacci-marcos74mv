3984f526ba69154cb2efbd02ba51840d48156368
Marcos Vinicius <128155251+marcos74mv@users.noreply.github.com>
2023-05-08 14:14:41 +0000

    Exercicio-Fibonacci

---
+++ main.py
@@ -0,0 +1,12 @@
+def fibonacci(n):
+    if n < 0:
+        raise ValueError("n tem que ser maior do que zero")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    return fibonacci(n-1) + fibonacci(n-2)
+
+
+n = int(input("Digite um número:"))
+print(f"Fibonacci({n}) = {fibonacci(n)}")
